583c019fee24c04a58109b03c083f4af3ac264a8
Go-Eun Park <dbtshfdl1@gmail.com>
2021-11-21 02:00:44 +0900

    url설정, 모듈화, 카테고리, 포스트 작성/수정

--- posting/models.py
+++ posting/models.py
@@ -2,17 +2,36 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/posting/category/{self.slug}'
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Post(models.Model):
     title=models.CharField(max_length=30)
     reliability=models.IntegerField()
     content=models.TextField()
+    head_image = models.ImageField(upload_to='posting/images/%Y/%m/%d/', blank=True)
+
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
+
     def get_absolute_url(self):
-        return f'/blog/{self.pk}'
+        return f'/posting/{self.pk}'
+
